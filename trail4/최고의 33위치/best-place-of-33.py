d5eab159b2f036f2b2609cb9b15518e4809a282a
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2026-07-12 18:00:40 +0900

    [Passed] 최고의 33위치 (55ms, 16MB)

--- trail4/최고의 33위치/best-place-of-33.py
+++ trail4/최고의 33위치/best-place-of-33.py
@@ -1,6 +1,18 @@
 N  = int(input())
 
+matrix = [list(map(int, input().split())) for i in range(N)]
 
-matrix = [[0] * N  for i in range(N)]
 
-print(matrix)
+MAX_COUNT = 0
+
+for i in range(N-3+1):
+    for j in range(N-3+1):
+        count = 0 
+        for di in range(3):
+            for dj in range(3):
+                if matrix[i+di][j+dj] == 1:
+                    count += 1
+        MAX_COUNT = max(MAX_COUNT, count)
+
+print(MAX_COUNT)
+
