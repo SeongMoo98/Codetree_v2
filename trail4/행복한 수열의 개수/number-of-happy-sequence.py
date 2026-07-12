636e5337f33082bdd9dd87221a4fb4dc866c657b
Lee Seong Moo <92712651+SeongMoo98@users.noreply.github.com>
2026-07-12 18:43:50 +0900

    [Wrong Answer] 행복한 수열의 개수 (68ms, 17MB)

--- trail4/행복한 수열의 개수/number-of-happy-sequence.py
+++ trail4/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,4 +1,33 @@
-n, m = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
+# 1 <= N <= 100
+# 행복한 수열 = 연속하여 M개 이상의 동일한 원소가 나오는 순간이 존재하는 수열
+# 각 행/열 마다 봤을 때 나오는 N개의 수열 중 행복한 수열의 개수를 세서 출력
 
-# Please write your code here.
+N, M = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+
+ans = 0
+
+if N == 1:
+    ans = 2
+else:
+    # 행방향
+    for i in range(N):
+        count = 1
+        for j in range(1, N):
+            if matrix[i][j-1] == matrix[i][j]:
+                count += 1
+            if count >= M:
+                ans += 1
+                break
+    # 열방향
+    for j in range(N):
+        count = 1
+        for i in range(1, N):
+            if matrix[i-1][j]== matrix[i][j]:
+                count += 1
+        
+            if count >= M:
+                ans += 1
+                break
+
+print(ans)
